275af4929b97448bea71d2cfa586899f48750ed4
Luke Walker <luwalker@microsoft.com>
2023-12-21 15:47:05 -0700

    Upload Day 2

---
+++ Day002/Main.py
@@ -0,0 +1,29 @@
+print("123" + "456")
+
+print(123 + 456)
+
+#String
+
+#Integer
+
+#Floating point number
+
+3.14159
+
+#Boolean
+
+True
+False
+
+#Can use type() to determine the type of variable you are using
+#str() typecasts to string
+#int() typecasts to integer
+#float() typecasts to float
+
+print(len(str(1234)))
+
+a = float(123)
+print(type(a))
+print(a)
+
+print(str(70) + str(100))
