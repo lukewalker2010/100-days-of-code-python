90b9b2bf8a848fd7b669e6b886ed6a4af757f0cf
Luke Walker <luwalker@microsoft.com>
2024-02-27 15:48:18 -0700

    Day 7 complete

--- Day007/main.py
+++ Day007/main.py
@@ -15,24 +15,41 @@ word_list = ["ardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
+end_of_game = False
+lives = 6
+stages = ["1", "2", "3", "4", "5", "6", "7"]
+logo = "Hangman"
+
+print(logo)
 
 #DEBUG
 print(f"Psst, the solution is {chosen_word}.")
 
-num_lives = 10
+display = []
+for _ in range(word_length):
+    display += "_"
 
-while num_lives < 10:
+while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
-    display = []
-    for _ in range(word_length):
-        display += "_"
+    if guess in display:
+        print(f"You've already guessed {guess}")
+
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that is not in the word. You lose a life")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
 
     for position in range(word_length):
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
 
-    print(display)
+    print(f"{' '.join(display)}")
 
-    num_lives += 1
+    if "_" not in display:
+        end_of_game = True
+        print("You win!")
+    print(stages[lives])
