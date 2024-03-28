37228d1c46e5e8b2b8065ef7f567a082c175aaf8
lukewalker2010 <luke.walker2010@gmail.com>
2024-03-27 20:45:33 -0600

    Day014 Start

---
+++ Day013/main.py
@@ -0,0 +1,10 @@
+# Day 13 Debugging
+
+# Describe the problem
+# Only gets to 19 as range 
+def my_function():
+    for i in range(1, 20):
+        if i == 20:
+            print("You got it")
+
+my_function()
