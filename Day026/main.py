eae3f4cceb1da9ed130cda97e3afda62fcd625f8
Luke Walker <luke.walker2010@gmail.com>
2024-04-30 15:17:56 -0600

    Day026

--- Day026/main.py
+++ Day026/main.py
@@ -34,3 +34,29 @@
 
 # passed_students = {student:score for (student, score) in students_scores.items() if score >= 60}
 # print(passed_students)
+
+# student_dict = {
+#     "student": ["Angela", "James", "Lilly"],
+#     "score": [56, 76, 98]
+# }
+
+# for (key, value) in student_dict.items():
+#     print(key)
+
+# import pandas
+# student_data_frame = pandas.DataFrame(student_dict)
+# print(student_data_frame)
+
+#Loop through a dataframe
+# for (key,value) in student_data_frame.items():
+#     print(value)
+
+# Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+    # print(index)
+    # print(row.student)
+    # print(row.score)
+    # if row.student == "Angela":
+    #     print(row.score)
+
+# {new_key:new_value for (index, row) in df.itterrows()}
